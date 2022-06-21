517d9b9bbcb4833df922cf3189162d8c4e260f4e
Болат <alamantrix@mail.ru>
2022-06-21 13:24:18 +0300

    Модуляция

---
+++ js/data.js
@@ -0,0 +1,95 @@
+import {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement} from './util.js';
+
+const MAX_ADS_COUNT = 10;
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const CHECKINS_CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const latitude = {
+min: 35.65000,
+max: 35.70000,
+};
+
+const longitude = {
+min: 139.70000,
+max: 139.80000,
+};
+
+const price = {
+min: 0,
+max: 10,
+};
+
+const rooms = {
+min: 0,
+max: 10,
+};
+
+const guests = {
+min: 0,
+max: 10,
+};
+
+const createObj = (id) => {
+  const lat = getRandomPositiveFloat(latitude.min, latitude.max);
+  const lng = getRandomPositiveFloat(longitude.min, longitude.max);
+
+  return {
+    author: {
+      avatar: `img/avatars/user${String(id).padStart(2, '0')}.png`
+    },
+    offer: {
+      title: 'Объявление',
+      address: `${lat}, ${lng}`,
+      price: getRandomPositiveInteger(price.min, price.max),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomPositiveInteger(rooms.min, rooms.max),
+      guests: getRandomPositiveInteger(guests.min, guests.max),
+      checkin: getRandomArrayElement(CHECKINS_CHECKOUTS),
+      checkout: getRandomArrayElement(CHECKINS_CHECKOUTS),
+      features:
+        FEATURES.slice(0, getRandomPositiveInteger(1, FEATURES.length)),
+      description: 'Описание помещения',
+      photos:
+      PHOTOS.slice(0, getRandomPositiveInteger(1, PHOTOS.length)),
+    },
+    location: {
+      lat,
+      lng,
+    }
+  };
+};
+
+const createAds = (amount) => 
+    Array.from(
+      { length: amount },
+        (_, index) => createObj(index));
+
+
+createAds(MAX_ADS_COUNT);
---
+++ js/util.js
@@ -0,0 +1,20 @@
+function getRandomPositiveInteger(a, b) {
+    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+    const result = Math.random() * (upper - lower + 1) + lower;
+  
+    return Math.floor(result);
+  }
+  
+  function getRandomPositiveFloat(a, b, digits = 5) {
+    const lower = Math.min(Math.abs(a), Math.abs(b));
+    const upper = Math.max(Math.abs(a), Math.abs(b));
+    const result = Math.random() * (upper - lower) + lower;
+  
+    return +result.toFixed(digits);
+  }
+  
+  const getRandomArrayElement = (elements) =>
+    elements [getRandomPositiveInteger(0, elements.length - 1)];
+  
+export {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement};
