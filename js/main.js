553e32126eb229b7d8614b3a33db3c662f2a5c12
bolatbairakov <106433780+bolatbairakov@users.noreply.github.com>
2022-06-21 14:31:19 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ max: 10,
 function getRandom (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  
+
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
@@ -68,7 +68,7 @@ getRandom();
 function getFractionRandom (a, b, digits = 5) {
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
-  
+
   const result = Math.random() * (upper - lower) + lower;
 
   return +result.toFixed(digits);
