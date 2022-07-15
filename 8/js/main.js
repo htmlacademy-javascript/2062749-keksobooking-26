9bc92162d07fdb1255e0c56b55c3e042ddf7e59c
Keks <keks@htmlacademy.ru>
2022-07-15 23:11:44 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,2 @@
+import './data.ls';
+import './util.js';
---
+++ 8/js/util.js
@@ -0,0 +1,22 @@
+function getRandom(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+function getFractionRandom(a, b, digits = 5) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+
+  const result = Math.random() * (upper - lower) + lower;
+
+  return +result.toFixed(digits);
+}
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandom(0, elements.length - 1)];
+
+export { getRandom, getFractionRandom, getRandomArrayElement };
